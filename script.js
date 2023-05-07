9b49f76af48d75c9536adb6f9c751fec0d6babab
Santiago Zeniquel <santi_zeniquel_2016@hotmail.com>
2023-05-07 18:51:38 -0300

    trabajo 1 JS

---
+++ script.js
@@ -0,0 +1,13 @@
+//TRABAJO PRACTICO N°1
+
+console.log("TRABAJO N°1")
+
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let diasDelMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+for(let i =0; i<meses.length; i++){
+    console.log(`Mes ${meses[i]} tiene ${diasDelMes[i]} dias`);
+}
+
+console.log("--------------------------------------------------------------")
