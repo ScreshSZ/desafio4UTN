612e4c3e7ff8495df94fe93200e92121674a6ea8
Santiago Zeniquel <santi_zeniquel_2016@hotmail.com>
2023-05-07 19:02:25 -0300

    se mejoro el codigo

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Ag
 
 let diasDelMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+
+console.log("LOS MESES QUE POSEEN 31 DIAS")
 for(let i =0; i<meses.length; i++){
-    console.log(`Mes ${meses[i]} tiene ${diasDelMes[i]} dias`);
+    if(diasDelMes[i] == 31){
+        console.log(`El mes ${meses[i]} posee ${diasDelMes[i]}`)
+    }
 }
 
 console.log("--------------------------------------------------------------")
+
+console.log("LOS MESES QUE POSEEN 30 DIAS")
+for(let i =0; i<meses.length; i++){
+    if(diasDelMes[i] == 30){
+        console.log(`El mes ${meses[i]} posee ${diasDelMes[i]}`)
+    }
+}
